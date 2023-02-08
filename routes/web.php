2073ca8e016f127a0cb71b30a157fee6659e2e5c
Rendy <renddevs@gmail.com>
2023-02-08 10:49:40 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,16 @@ use App\Http\Controllers\StockController;
 // });
 
 Route::get('/stock', [StockController::class, 'index']);
-
 Route::post('/stock/create', [StockController::class, 'create']);
-
 // Route::get('/stock/', function(){
 //     return view('stock.list');
 // });
-
 Route::get('/stock/add', function(){
     return view('stock.add');
 });
-
-
-
 // Route::get('/stock/edit/', function(){
 //     return view('stock.edit');
 // });
-
 Route::get('/stock/edit/{id}', [StockController::class, 'edit']);
 
 
